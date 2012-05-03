c42a4b0fa1e4cf3c62ffb5a3daf11de765fbbc7e
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2012-05-03 21:45:28 +0200

    dunno

--- ingen.lua
+++ ingen.lua
@@ -1,5 +1,6 @@
 require "subprocess"
 require "os"
+require "io"
 require "socket"
 
 local sleep = 
@@ -7,24 +8,30 @@ local sleep =
 		socket.select(nil, nil, sec)
 	end
 
+local ingen_cmd = "ingenish" 
+
 local run = 
 	function(s)
-		local engine = subprocess.popen({"/usr/local/bin/ingen", "-eg"})
+		local p = { "ingen", "-e"}
+		p.stdout = subprocess.PIPE
+		p.stderr = subprocess.stdout
+		local engine = subprocess.popen(p)
+		outlines = engine.stdout:lines()
 		sleep(2)
 		-- create nodes first
 		for k, object  in pairs(s) do
 			if nil ~= object.uri then
 				object.key_for_port_lookup = k
-				local cmd = "ingen_cmd put /" .. k .. " 'a ingen:Node ; ingen:prototype <" .. object.uri ..">'"
+				local cmd = ingen_cmd .. " put /" .. k .. " 'a ingen:Node ; ingen:prototype <" .. object.uri ..">'"
 				print("executing " .. cmd)
 				
 				os.execute(cmd)
 				sleep(0.01)
 
-				os.execute("ingen_cmd put /left_in 'a lv2:InputPort ; a lv2:AudioPort'")
+				os.execute(ingen_cmd .. " put /left_in 'a lv2:InputPort ; a lv2:AudioPort'")
 				sleep(0.01)
 
-				os.execute("ingen_cmd put /left_out 'a lv2:OutputPort ; a lv2:AudioPort'")
+				os.execute(ingen_cmd .. " put /left_out 'a lv2:OutputPort ; a lv2:AudioPort'")
 				sleep(0.01)
 			end
 		end
@@ -33,13 +40,13 @@ local run =
 		if nil ~= s.wires then
 			for k, o in pairs(s.wires) do
 
-				local cmd = "ingen_cmd connect /" .. k.plugin.key_for_port_lookup .. "/" .. k.symbol .. " " .. " /" .. o.plugin.key_for_port_lookup .. "/" .. o.symbol
+				local cmd = ingen_cmd .. " connect /" .. k.plugin.key_for_port_lookup .. "/" .. k.symbol .. " " .. " /" .. o.plugin.key_for_port_lookup .. "/" .. o.symbol
 				print("executing " .. cmd)
 				
 				os.execute(cmd)
 				sleep(0.01)
 
-				local cmd = "ingen_cmd connect /" .. k.plugin.key_for_port_lookup .. "/" .. k.symbol .. " /left_out"
+				local cmd = ingen_cmd .. " connect /" .. k.plugin.key_for_port_lookup .. "/" .. k.symbol .. " /left_out"
 				print("executing " .. cmd)
 				
 				os.execute(cmd)
@@ -53,9 +60,15 @@ local run =
 		sleep(2)
 	end
 
+local stop =
+	function(s)
+		
+	end
+
 _G.ingen = 
 	function()
 		synth = {}
+
 		local mt = {}
 		mt.__index =
 			function(s, key)
@@ -64,6 +77,8 @@ _G.ingen =
 				end
 			end
 		setmetatable(synth, mt)
+
 		synth.wires = {}
+
 		return synth
 	end
